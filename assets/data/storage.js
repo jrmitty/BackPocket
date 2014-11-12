06e12eb5803d5d3eae90e8d609284bc6c1ae9d8a
Nicholas Urban <urban.miklosjr@gmail.com>
2014-11-12 19:15:04 +0000

    storage function

---
+++ assets/data/storage.js
@@ -0,0 +1,48 @@
+define([
+	'../namespace'
+], function(namespace) {
+	'use strict';
+
+	/**
+	 * Storage module for support local data handling.
+	 * @module Storage
+	 * @return new Storage
+	 */
+
+	/**
+	 * @class Storage
+	 * @constructor
+	 */
+	function Storage() {}
+
+	/**
+	 * Setting data into browser's local storage
+	 * @method setData
+	 * @param {String} key Key for stored value
+	 * @param {Object} data JSON object will going to stringify and store as String
+	 * @requires compatibility.polyfill.localstorage
+	 * @default {Boolean} true
+	 * @return {Boolean} true
+	 */
+	Storage.prototype.setData = function(key, data) {
+		localStorage.setItem(key, JSON.stringify(data));
+		return true;
+	};
+
+	/**
+	 * Getting data from browser's local storage
+	 * @method getData
+	 * @param {String} key Key for stored value
+	 * @requires compatibility.polyfill.localstorage
+	 * @return {Object|Boolean} JSON parsed object | false
+	 */
+	Storage.prototype.getData = function(key) {
+		if (localStorage.getItem(key)) {
+			return JSON.parse(localStorage.getItem(key));
+		}
+		return false;
+	};
+
+	namespace.Storage = new Storage();
+	return namespace.Storage;
+});
